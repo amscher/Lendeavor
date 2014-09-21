3f404bbf722a1a76d980df77020a4dd8ef3eb9f1
Montana Scher <amscher@gmail.com>
2014-09-21 15:04:50 -0700

    added animation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,9 @@ angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
 
   }])
-  .controller('LoanTypesCtrl', ['$scope', 'LoanTypes', 'LoanAspects', function($scope, LoanTypes, LoanAspects) {
+  .controller('LoanTypesCtrl', ['$scope', '$anchorScroll', '$location',
+      'LoanTypes', 'LoanAspects',
+      function($scope, $anchorScroll, $location, LoanTypes, LoanAspects) {
     // Initialization.
     $scope.finished = false;
     $scope.selected = [true, false, false];
@@ -43,5 +45,52 @@ angular.module('myApp.controllers', [])
           "filter" : ""
         }];
 
+   $scope.scrollTo = function(id) {
+        // This scrolling function
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+
+        var startY = currentYPosition();
+        var stopY = elmYPosition(id);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i=startY; i>stopY; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop)
+                return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+
+        function elmYPosition(id) {
+            var elm = document.getElementById(id);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            } return y;
+        }
+   }
 
   }]);
